f8aeaf75b41139a32a8ed43321eb127ca994292d
Satel14 <ostaplvov@gmail.com>
2021-06-03 19:45:07 +0300

    zxc

---
+++ frontend/src/components/Body.js
@@ -0,0 +1,237 @@
+import React, { Component } from 'react'
+
+
+import './css/Body.css'
+
+
+export class Body extends Component {
+    render() {
+        return (
+            <div id="best" className="section section-md best novi-background bg-cover">
+    <div className="container">
+      <div className="section-title-md">
+        BEST OFFERS FROM AUTOCLUB
+      </div>
+      <div className="tabs-custom tabs-horizontal tabs-corporate" id="tabs-1">
+        <ul className="nav nav-tabs">
+          <li className="active" role="presentation">
+            <a href="#tabs-1-1" data-toggle="tab">Most researched manufacturers</a>
+          </li>
+          <li role="presentation">
+            <a href="#tabs-1-2" data-toggle="tab">Latest vehicles on sale</a>
+          </li>
+        </ul>
+        <div className="tab-content">
+          <div className="tab-pane fade in active" id="tabs-1-1">
+            <div className="row row-fix">
+              <div className="col-sm-12 col-md-3">
+                <ul className="product-filter">
+                  <li>
+                    <a href="#">All manufacturers</a>
+                  </li>
+                  <li>
+                    <a href="#">ASTON MARTIN</a>
+                  </li>
+                  <li>
+                    <a href="#">ALPHA ROMEO</a>
+                  </li>
+                  <li>
+                    <a href="#">AUDI</a>
+                  </li>
+                  <li>
+                    <a href="#">BMW</a>
+                  </li>
+                  <li>
+                    <a href="#">LAND ROVER</a>
+                  </li>
+                  <li>
+                    <a href="#">Mercedes-Benz</a>
+                  </li>
+                  <li>
+                    <a href="#">PORSCHE</a>
+                  </li>
+                  <li>
+                    <a href="#">SUZUKI</a>
+                  </li>
+                  <li>
+                    <a href="#">TOYOTA</a>
+                  </li>
+                  <li>
+                    <a href="#">VOLVO</a>
+                  </li>
+                  <li>
+                    <a href="#">VOLKSWAGEN</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-sm-12 col-md-9">
+                <div className="row">
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-4-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2015</p>
+                          <div className="product-minimal-title"><a href="details.html"> BMW M6 Sport Hybrid</a></div>
+                          <div className="info">
+                            <span className="price">24,380</span>
+                            <span className="speed">35,000 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">White</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-5-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2016</p>
+                          <div className="product-minimal-title"><a href="details.html"> 2016 Ferrari Testarosa</a></div>
+                          <div className="info">
+                            <span className="price">95,900</span>
+                            <span className="speed">99,000 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">Red</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-6-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2015</p>
+                          <div className="product-minimal-title"><a href="details.html"> 2016 Bugatti Veyron</a></div>
+                          <div className="info">
+                            <span className="price">98,995</span>
+                            <span className="speed">95,000 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">Blue</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-7-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2017</p>
+                          <div className="product-minimal-title"><a href="details.html"> 2017 Lexus-AMG C63</a></div>
+                          <div className="info">
+                            <span className="price">31,900</span>
+                            <span className="speed">12,000 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">Red</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-8-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2016</p>
+                          <div className="product-minimal-title"><a href="details.html"> Mercedes - AMG</a></div>
+                          <div className="info">
+                            <span className="price">18,995</span>
+                            <span className="speed">52,000 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">Yellow</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-xxs-12 col-xs-6 col-sm-4">
+                    <div className="product-minimal">
+                      <div className="thumbnail clearfix">
+                        <figure className="product-minimal-img">
+                          <a href="details.html">
+                            <img src="http://livedemo00.template-help.com/wt_prod-14633/images/home-9-270x150.jpg" alt="" className="img-responsive"></img> </a>
+                        </figure>
+                        <div className="caption">
+                          <p className="small">REGISTERED 2017</p>
+                          <div className="product-minimal-title"><a href="details.html"> 2017 Mercedes - AMG</a></div>
+                          <div className="info">
+                            <span className="price">64,380</span>
+                            <span className="speed">210 KM</span>
+                          </div>
+                          <ul className="tag-list">
+                            <li><a href="#">Used</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">Automatic</a></li>
+                            <li><a href="#">Black</a></li>
+                            <li><a href="#">Petrol</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className="tab-pane fade" id="tabs-1-2">
+            <div className="row row-fix">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+        )
+    }
+}
+
+export default Body
